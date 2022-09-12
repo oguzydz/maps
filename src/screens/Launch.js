ac73ef545ca9f801adb8ab2731c973945331f3ac
Oğuz Yıldız <ydz.oguz@gmail.com>
2022-09-12 23:42:04 +0300

    Add Button component to navigate for map

--- src/screens/Launch.js
+++ src/screens/Launch.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import mobileCaseData from "../assets/json/mobileCaseData";
+
+import Button from '../components/Button';
 
 const Launch = () => {
     return (
-        <View>
-            <Text>Launch</Text>
+        <View style={styles.container}>
+            <SafeAreaView style={styles.menuContainer}>
+                {mobileCaseData?.regions?.map((item, index) => <Button key={index.toString()} region={item} />)}
+            </SafeAreaView>
         </View>
     )
 };
 
 export default Launch;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#e8e8e8',
+        justifyContent: "center",
+    },
+    menuContainer: {
+        paddingHorizontal: 32,
+    }
+});
