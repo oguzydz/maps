f9405cd9c275826e13916087428f83267869e733
Oğuz Yıldız <ydz.oguz@gmail.com>
2022-09-12 19:23:15 +0300

    Setup react navigation with inital screens

---
+++ src/AppContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//Screens
+import Launch from './screens/Launch';
+import Map from './screens/Map';
+
+const Stack = createNativeStackNavigator();
+
+const AppContainer = () => (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Group screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Launch" component={Launch} />
+                <Stack.Screen name="Map" component={Map} />
+            </Stack.Group>
+        </Stack.Navigator>
+    </NavigationContainer>
+);
+
+export default AppContainer
+
---
+++ src/screens/Launch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Launch = () => {
+    return (
+        <View>
+            <Text>Launch</Text>
+        </View>
+    )
+};
+
+export default Launch;
